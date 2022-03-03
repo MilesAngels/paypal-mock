8a91e5601df458f8f9b44675d5f8496bb03bc52e
MilesAngels <mangels@alphaworks.tech>
2022-03-03 11:53:57 -0800

    responsiveness

---
+++ assets/js/responsive.js
@@ -0,0 +1,7 @@
+//Made to make DOM manipulations based on screen sizes
+let new_features = document.getElementById('new-features');
+let get_app = document.getElementById('get-app');
+
+if (screen.width < 600) {
+    get_app.innerText = "Get the app";
+}
